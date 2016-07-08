14583796664df55e36dbbc7774bcbc69f02e1e30
David <email": null>
2016-07-07 19:14:01 -0700

    moved the routes for home movie_single and notfound to index page and required it on the app.js and acces them

--- app.js
+++ app.js
@@ -3,26 +3,18 @@ var app = express();
 
 
 app.set('view engine','ejs');
+var routes = require('./routes');
 ///routes
 
 //home
 
-app.get('/', function(req, res){
-  res.render('home',{
-    title : "Star Wars Movies"
-  });
-});
+app.get('/', routes.home);
 
 //movie single
-app.get('/star_wars_episode/:episode_number?', function(req, res){
-  var episode_number = req.params.episode_number;
-  res.send("This is the page for episode " + episode_number);
-});
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 
 //notFound
-app.get('*', function(req, res){
-  res.send("this is not the pafe that you are looking for")
-})
+app.get('*',routes.notFound);
 
 app.listen(3000, function(){
   console.log("The app is  running on locallhost")
